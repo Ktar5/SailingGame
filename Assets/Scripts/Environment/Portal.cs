f5085788093bc010af2942a4161ac197393a0c21
biokuta7 <hatmelders@gmail.com>
2017-04-05 14:02:53 -0700

    Portals v2
    added a screen-letterbox effect. still needs optimization and prettying
    up.

--- Assets/Scripts/Environment/Portal.cs
+++ Assets/Scripts/Environment/Portal.cs
@@ -7,17 +7,93 @@ public class Portal : MonoBehaviour {
 	public Portal otherPortal;
 	public TopDown2DPlayer.Direction facingDirection;
 
+	[Range(0.0f, 100.0f)]
+	private float letterboxFillPercent = 0.0f;
+	private Texture2D letterbox;
+	private Rect letterboxRect;
 
-	void OnTriggerEnter2D(Collider2D other) {
+	private bool playerInPortal = false;
+	private bool fading = false;
+	private bool transporting = false;
+	private TopDown2DPlayer player;
 
-		TopDown2DPlayer player = other.GetComponent<TopDown2DPlayer> ();
+	void Start() {
+		InitGUILetterbox ();
+		InitPlayer ();
+	}
 
-		if (player != null) {
-			player.transform.position = otherPortal.transform.position + (2.0f * PortalOffsetFromDirection(otherPortal.facingDirection));
-			player.movement.direction = otherPortal.facingDirection;
+	void Update() {
+
+		CheckInPortal ();
+		Fade ();
+		StartCoroutine (Transport());
+
+	}
+
+	void InitPlayer() {
+		player = GameObject.FindGameObjectWithTag("Player").GetComponent<TopDown2DPlayer> ();
+	}
+
+	void CheckInPortal() {
+		if (playerInPortal && !fading && !transporting) {
+
+			Debug.Log ("[E] Advance");
+
+			if (Input.GetKeyDown (KeyCode.E)) {
+
+				fading = true;
+
+			}
+
+		}
+	}
+
+	void Fade() {
+
+
+		letterboxFillPercent = Mathf.Lerp (letterboxFillPercent,(fading ? 100.0f : 0.0f), Time.deltaTime * 10.0f);
+
+		if (fading && letterboxFillPercent > 99.9f) {
+
+			transporting = true;
 
 		}
 
+	}
+
+	IEnumerator Transport() {
+		if (transporting && player != null) {
+			player.transform.position = otherPortal.transform.position + (2.0f * PortalOffsetFromDirection (otherPortal.facingDirection));
+			player.movement.direction = otherPortal.facingDirection;
+			yield return new WaitForSeconds(1.0f);
+			fading = false;
+			transporting = false;
+		}
+
+	}
+
+	void InitGUILetterbox() {
+		letterboxRect = new Rect (0, 0, 0, Screen.height);
+		letterbox = new Texture2D (1, 1);
+		letterbox.SetPixel (0, 0, Color.black);
+		letterbox.Apply ();
+
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+
+		if (other.name == "Player") {
+			playerInPortal = true;
+		}
+
+
+	}
+
+	void OnTriggerExit2D(Collider2D other) {
+		
+		if (other.name == "Player") {
+			playerInPortal = false;
+		}
 	}
 
 	Vector3 PortalOffsetFromDirection(TopDown2DPlayer.Direction dir) {
@@ -43,6 +119,13 @@ public class Portal : MonoBehaviour {
 			return new Vector3 (0.7f, -0.7f, 0);
 
 		}
+
+	}
+
+	void OnGUI() {
+
+		letterboxRect.width = (letterboxFillPercent * 0.01f * Screen.width);
+		GUI.DrawTexture (letterboxRect, letterbox);
 
 	}
 
